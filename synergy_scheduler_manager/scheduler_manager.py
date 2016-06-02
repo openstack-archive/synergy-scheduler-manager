8f37ac532a725b30ab623b1c8066d94ccac2fa1b
Vincent Llorens <vincent.llorens@cc.in2p3.fr>
2016-06-02 16:37:52 +0200

    various clean & fix w/ changes in functionalities
    Fixes not altering funtionalities:
    - add missing requirement (requests)
    - fix methods called with arguments when its signature doesn't allow it
    - fix __init__ calls that didn't use `super()`
    - fix string interpolations with wrong arguments
    
    Affecting functionalities:
    - remove the TRUST command in the keystone manager since the trust method
      doesn't exist
    - add variable affectation in queue_manager.py:setData
    - remove unused method parameters in some methods in quota_manager.py and
      scheduler_manager.py
    
    Change-Id: I94655b276944b54922f88538879603c4fd72a11f

--- synergy_scheduler_manager/scheduler_manager.py
+++ synergy_scheduler_manager/scheduler_manager.py
@@ -40,7 +40,7 @@ class Notifications(object):
 
         self.dynamic_quota = dynamic_quota
 
-    def info(self, ctxt, publisher_id, event_type, payload, metadata):
+    def info(self, event_type, payload):
         LOG.debug("Notification INFO: event_type=%s payload=%s"
                   % (event_type, payload))
 
@@ -81,16 +81,13 @@ class Notifications(object):
             except Exception as ex:
                 LOG.warn("Notification INFO: %s" % ex)
 
-    def warn(self, ctxt, publisher_id, event_type, payload, metadata):
-        # LOG.info("Notification WARN: event_type=%s payload=%s metadata=%s"
-        #          % (event_type, payload, metadata))
-
+    def warn(self, event_type, payload):
         state = payload["state"]
         instance_id = payload["instance_id"]
         LOG.info("Notification WARN: event_type=%s state=%s instance_id=%s"
                  % (event_type, state, instance_id))
 
-    def error(self, ctxt, publisher_id, event_type, payload, metadata):
+    def error(self, event_type, payload, metadata):
         LOG.info("Notification ERROR: event_type=%s payload=%s metadata=%s"
                  % (event_type, payload, metadata))
 
